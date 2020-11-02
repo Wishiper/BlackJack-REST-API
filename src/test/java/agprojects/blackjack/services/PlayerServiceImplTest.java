0638e60e9970ce37d2fe4414a584c552c816e4fc
Alexander Georgiev <wishiper@gmail.com>
2020-11-02 14:30:31 +0200

    Added unit tests for getPlayerById both scenarios

--- src/test/java/agprojects/blackjack/services/PlayerServiceImplTest.java
+++ src/test/java/agprojects/blackjack/services/PlayerServiceImplTest.java
@@ -1,20 +1,28 @@
 package agprojects.blackjack.services;
 
+import agprojects.blackjack.exceptions.ApiRequestException;
 import agprojects.blackjack.models.Player;
 import agprojects.blackjack.models.Table;
 import agprojects.blackjack.models.dto.PlayerDTO;
 import agprojects.blackjack.repositories.PlayerRepository;
 import agprojects.blackjack.services.base.HandService;
 import agprojects.blackjack.utilities.CustomModelMapper;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.rules.ExpectedException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,7 +39,6 @@ class PlayerServiceImplTest {
 
     @InjectMocks
     Table table;
-
 
     @InjectMocks
     private static PlayerServiceImpl playerService = new PlayerServiceImpl();
@@ -58,7 +65,34 @@ class PlayerServiceImplTest {
     }
 
     @Test
-    void getPlayerById() {
+    void getPlayerById_ShouldRetrieveTheRightPlayerFromPlayerRepository() {
+        int playerId = 1;
+
+        Player player = new Player();
+        player.setPlayerId(playerId);
+        player.setName("name");
+
+        when(playerRepository.findById(playerId)).thenReturn(Optional.of(player));
+
+        Player playerResult = playerService.getPlayerById(playerId);
+
+        Assert.assertEquals(player,playerResult);
+    }
+
+    @Test
+    void getPlayerById_ShouldThrowApiRequestException_WhenPlayerIsNotFound() throws ApiRequestException {
+        int playerId = 1;
+
+        when(playerRepository.findById(playerId)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(ApiRequestException.class, () -> {
+            playerService.getPlayerById(playerId);
+        });
+
+        String expectedMessage = "Player with id: 1 was not found";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 
     @Test
